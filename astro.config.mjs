cc6906a25c520aa7fc7382aa7623678a0ae437fb
ER <eino.rauhala@proton.me>
2023-02-20 19:59:00 +0200

    feat: basic cards & typed yml for images +prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
+import yaml from '@rollup/plugin-yaml';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), svelte()],
+  vite: {
+    plugins: [yaml()]
+  }
 });
---
+++ src/components/ImageCard.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import type { Image } from '@ts/types';
+    export let image : Image;
+</script>
+
+<div class="bg-slate-500 p-2 text-white">
+    {image.name}
+</div>
