cebad57503efa8aa83a83a1ac7a41c17bb1429bc
ER <eino.rauhala@proton.me>
2023-02-21 18:41:51 +0200

    feat: barebones image card

--- src/components/ImageCard.svelte
+++ src/components/ImageCard.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
-  import type { Image } from "@ts/types";
+  import { templateStr } from "@ts/templateStrings";
+  import type { Edition, Image } from "@ts/types";
   export let image: Image;
 </script>
 
-<div class="bg-slate-500 p-2 text-white">
-  {image.name}
-</div>
+{#each image.editions as edition}
+  <div class="flex flex-col border-4 border-solid border-indigo-800 p-2">
+    <h2 class="mr-5 text-xl font-bold text-indigo-800">
+      {templateStr(image.name, { edition: edition })}
+    </h2>
+    <span>{image.creator}</span>
+  </div>
+{/each}
