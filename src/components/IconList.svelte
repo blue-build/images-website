3f7631963e239a42c9bc3695dff9a6169d30268e
ER <eino.rauhala@proton.me>
2023-02-23 21:17:19 +0200

    feature(dx): use an array of icon objects for gen

--- src/components/IconList.svelte
+++ src/components/IconList.svelte
@@ -1,53 +1,70 @@
 <script lang="ts">
   import { Feature, Desktop, Edition, Image } from "@ts/types";
 
-  import Icon from "@iconify/svelte";
+  import Icon, { IconifyIcon } from "@iconify/svelte";
+  
   import gnomeIcon from "@iconify/icons-simple-icons/gnome";
   import kdeIcon from "@iconify/icons-simple-icons/kde";
-  import xfceIcon from '@iconify/icons-simple-icons/xfce';
-  import genericWmIcon from '@iconify/icons-material-symbols/grid-view';
-  import noDeIcon from '@iconify/icons-material-symbols/close-rounded';
+  import xfceIcon from "@iconify/icons-simple-icons/xfce";
+  import genericWmIcon from "@iconify/icons-material-symbols/grid-view";
+  import noDeIcon from "@iconify/icons-material-symbols/close-rounded";
   import nvidiaIcon from "@iconify/icons-simple-icons/nvidia";
   import distroboxIcon from "@iconify/icons-simple-icons/hackthebox";
 
   export let image: Image;
   export let edition: Edition;
+
+  interface IconData {
+    condition: boolean;
+    icon: IconifyIcon;
+    tooltip: string;
+  }
+
+  const icons: Array<IconData> = [
+    {
+      condition: edition.desktop === Desktop.gnome,
+      icon: gnomeIcon,
+      tooltip: "Uses GNOME by default.",
+    },
+    {
+      condition: edition.desktop === Desktop.kde,
+      icon: kdeIcon,
+      tooltip: "Uses KDE Plasma by default.",
+    },
+    {
+      condition: edition.desktop === Desktop.xfce,
+      icon: xfceIcon,
+      tooltip: "Uses XFCE by default.",
+    },
+    {
+      condition: edition.desktop === Desktop.sway,
+      icon: genericWmIcon,
+      tooltip: "Uses SwayWM by default.",
+    },
+    {
+      condition: edition.desktop === Desktop.none,
+      icon: noDeIcon,
+      tooltip: "Doesn't include any desktop environment or window manager.",
+    },
+    {
+      condition: image.featureSet.includes(Feature.nvidia),
+      icon: nvidiaIcon,
+      tooltip: "Includes Nvidia drivers.",
+    },
+    {
+      condition: image.featureSet.includes(Feature.distrobox),
+      icon: distroboxIcon,
+      tooltip: "Includes distrobox.",
+    },
+  ];
 </script>
 
 <div class="mt-4 mb-2 flex gap-2 pl-2">
-  {#if edition.desktop === Desktop.gnome}
-    <span title="Uses GNOME by default.">
-      <Icon icon={gnomeIcon} width="24" />
-    </span>
-  {/if}
-  {#if edition.desktop === Desktop.kde}
-    <span title="Uses KDE Plasma by default.">
-      <Icon icon={kdeIcon} width="24" />
-    </span>
-  {/if}
-  {#if edition.desktop === Desktop.xfce}
-    <span title="Uses XFCE by default.">
-        <Icon icon={xfceIcon} width="24" />
-    </span>
+  {#each icons as icon}
+    {#if icon.condition}
+      <span title={icon.tooltip}>
+        <Icon icon={icon.icon} width="24" />
+      </span>
     {/if}
-    {#if edition.desktop === Desktop.sway}
-    <span title="Uses SwayWM by default.">
-        <Icon icon={genericWmIcon} width="24" />
-    </span>
-    {/if}
-    {#if edition.desktop === Desktop.none}
-    <span title="Doesn't include any desktop environment or window manager.">
-        <Icon icon={noDeIcon} width="24" />
-    </span>
-    {/if}
-  {#if image.featureSet.includes(Feature.nvidia)}
-    <span title="Includes Nvidia drivers.">
-      <Icon icon={nvidiaIcon} width="24" />
-    </span>
-  {/if}
-  {#if image.featureSet.includes(Feature.distrobox)}
-    <span title="Includes distrobox.">
-      <Icon icon={distroboxIcon} width="24" />
-    </span>
-  {/if}
+  {/each}
 </div>
