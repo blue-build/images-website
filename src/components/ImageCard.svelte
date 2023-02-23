9013ee6cf4e13353ba6e5eef5ee7d43cffb1b75c
ER <eino.rauhala@proton.me>
2023-02-23 11:26:28 +0200

    style: format using prettier

--- src/components/ImageCard.svelte
+++ src/components/ImageCard.svelte
@@ -2,18 +2,20 @@
   import { getGithubRepository } from "@ts/apiHelpers";
   import { templateStr } from "@ts/templateStrings";
   import type { Image } from "@ts/types";
-  import Icon from '@iconify/svelte';
-  import starRounded from '@iconify/icons-material-symbols/star-rounded';
+  import Icon from "@iconify/svelte";
+  import starRounded from "@iconify/icons-material-symbols/star-rounded";
   import { onMount } from "svelte";
   export let image: Image;
 
   // TODO figure out how to async
 
-  let githubRepo : Object;
-  
-  onMount(async () =>  {
-    githubRepo = await getGithubRepository(image.githubRepo).then((data) => data);
-  })
+  let githubRepo: Object;
+
+  onMount(async () => {
+    githubRepo = await getGithubRepository(image.githubRepo).then(
+      (data) => data
+    );
+  });
 </script>
 
 {#each image.editions as edition}
@@ -22,13 +24,15 @@
       <h2 class="mr-8 text-xl font-bold text-indigo-800">
         {templateStr(image.name, { edition: edition })}
       </h2>
-      <div class="flex justify-self-end self-center items-center text-indigo-800 font-bold">
-        <Icon icon={starRounded}/>
+      <div
+        class="flex items-center self-center justify-self-end font-bold text-indigo-800"
+      >
+        <Icon icon={starRounded} />
         {#if githubRepo}
-        {githubRepo["stargazers_count"]}
+          {githubRepo["stargazers_count"]}
         {/if}
       </div>
     </div>
-    <span>{image.creator}</span> 
+    <span>{image.creator}</span>
   </div>
 {/each}
