bdba57626db94154701d7d907c3a646f86dd1be0
ER <eino.rauhala@proton.me>
2023-02-24 18:22:30 +0200

    feat(dx): componentize image listing

---
+++ src/sections/ImageList.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import ImageCard from "@components/ImageCard.svelte";
+
+  import type { Images } from "@ts/types";
+  import imagesUntyped from "@content/images.yml";
+  const images: Images = imagesUntyped;
+</script>
+
+<div class="flex flex-wrap gap-2 p-4">
+  <ImageCard image={images.images[0]} />
+</div>
