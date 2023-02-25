8bf8e156229cf35c2c128ae4be420c4140cc696a
ER <eino.rauhala@proton.me>
2023-02-25 12:23:46 +0200

    feat: filtering using featuresets

---
+++ src/components/Checkbox.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let checked: boolean = false;
+  export let change = (e: Event) => {};
+
+  let uid = Math.random().toString(16).slice(2);
+</script>
+
+<input
+  type="checkbox"
+  id={`checkbox-${uid}`}
+  class="hidden"
+  bind:checked
+  on:change={change}
+/>
+<label for={`checkbox-${uid}`} class="group" class:isChecked={checked}>
+  <slot />
+</label>
--- src/sections/ImageList.svelte
+++ src/sections/ImageList.svelte
@@ -1,24 +1,78 @@
 <script lang="ts">
   import ImageCard from "@components/ImageCard.svelte";
+  import Box from "@components/Box.svelte";
+  import Checkbox from "@components/Checkbox.svelte";
 
-  import { ImageFilter, Images } from "@ts/types";
+  import Icon from "@iconify/svelte";
+  import type { IconifyIcon } from "@iconify/svelte";
+  import nvidiaIcon from "@iconify/icons-simple-icons/nvidia";
+  import distroboxIcon from "@iconify/icons-simple-icons/hackthebox";
+
+  import type { ImageFilter, Images } from "@ts/types";
+  import { Feature } from "@ts/types";
+
   import imagesUntyped from "@content/images.yml";
   const images: Images = imagesUntyped;
 
-  $: filteredImages = images.images.filter((i) => {
-    if (filter.featureSet.length < 1 && filter.desktop.length < 1) return true;
-
-    if (filter.featureSet.length > 0) {
-      return filter.featureSet.every((f) => i.featureSet.includes(f));
-    }
-    return true;
-  });
+  $: filteredImages = filterImages(filter);
+  function filterImages(filter: ImageFilter) {
+    return images.images.filter((i) => {
+      if (filter.featureSet.length < 1 && filter.desktop.length < 1)
+        return true;
+      if (filter.featureSet.length > 0) {
+        return filter.featureSet.every((f) => i.featureSet.includes(f));
+      }
+      return true;
+    });
+  }
 
   let filter: ImageFilter = {
     featureSet: [],
     desktop: [],
   };
+
+  interface FeatureButton {
+    text: string;
+    feature: Feature;
+    icon: IconifyIcon;
+  }
+  const featureButtons: Array<FeatureButton> = [
+    {
+      text: "Nvidia",
+      feature: Feature.nvidia,
+      icon: nvidiaIcon,
+    },
+    {
+      text: "Distrobox",
+      feature: Feature.distrobox,
+      icon: distroboxIcon,
+    },
+  ];
 </script>
+
+<div
+  class="m-4 flex flex-row gap-2 bg-indigo-100 max-w-4xl border-4 border-indigo-900 p-3"
+>
+  {#each featureButtons as fb}
+    <Checkbox
+      change={(e) => {
+        if (e.target?.checked) {
+          filter.featureSet = [...filter.featureSet, fb.feature];
+        } else {
+          filter.featureSet = filter.featureSet.filter((f) => f !== fb.feature);
+        }
+        console.log(fb.feature);
+      }}
+    >
+      <Box
+        class="flex flex-row gap-2 group-[.isChecked]:bg-indigo-300 group-[.isChecked]:outline group-[.isChecked]:outline-indigo-500 group-[.isChecked]:hover:outline-indigo-400 outline-2"
+      >
+        <Icon icon={fb.icon} class="self-center" />
+        {fb.text}
+      </Box>
+    </Checkbox>
+  {/each}
+</div>
 
 <div class="flex flex-wrap gap-2 p-4">
   {#each filteredImages as image}
