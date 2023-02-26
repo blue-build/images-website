ca18ff2fab82dfb5988fc3b000b9902e6b765faf
ER <eino.rauhala@proton.me>
2023-02-26 18:48:00 +0200

    feat: darkmode store which box reads from

--- src/components/Box.svelte
+++ src/components/Box.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
+  import { darkMode } from "@ts/stores";
   export let dark: boolean = false;
 
   let defaultClasses: string;
-  if (dark) {
+  if (dark || darkMode) {
     defaultClasses =
       "bg-gray-900 outline outline-2 p-2 text-white hover:outline-indigo-300 focus:outline-indigo-300 outline-indigo-100 dark:outline-indigo-900 dark:hover:outline-indigo-600 dark:focus:outline-indigo-600";
   } else {
