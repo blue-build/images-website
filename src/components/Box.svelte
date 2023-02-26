2fa6908fdc2af6d98417528402a9da69a58ebc09
ER <eino.rauhala@proton.me>
2023-02-26 18:59:50 +0200

    feat(styling): make dark mode box colors unified

--- src/components/Box.svelte
+++ src/components/Box.svelte
@@ -2,13 +2,13 @@
   import { darkMode } from "@ts/stores";
   export let dark: boolean = false;
 
-  let defaultClasses: string;
+  let defaultClasses: string = "outline outline-2 p-2";
   if (dark || darkMode) {
-    defaultClasses =
-      "bg-gray-900 outline outline-2 p-2 text-white hover:outline-indigo-300 focus:outline-indigo-300 outline-indigo-100 dark:outline-indigo-900 dark:hover:outline-indigo-600 dark:focus:outline-indigo-600";
+    defaultClasses +=
+      " bg-slate-800 text-white hover:outline-indigo-300 focus:outline-indigo-300 outline-indigo-100 dark:outline-indigo-900 dark:hover:outline-indigo-600 dark:focus:outline-indigo-600";
   } else {
-    defaultClasses =
-      "bg-gray-100 outline outline-2 p-2 text-black hover:outline-indigo-300 focus:outline-indigo-300 outline-indigo-100 dark:outline-indigo-900 dark:hover:outline-indigo-600 dark:focus:outline-indigo-600";
+    defaultClasses +=
+      " bg-gray-100 text-black hover:outline-indigo-300 focus:outline-indigo-300 outline-indigo-100 dark:outline-indigo-900 dark:hover:outline-indigo-600 dark:focus:outline-indigo-600";
   }
 </script>
 
