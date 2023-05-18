a46cde3471917717340ee85b590c560be90965aa
ER <eino.rauhala@proton.me>
2023-05-18 14:49:36 +0300

    feat: basic "imagechooser" w list of editions no functionality yet, but styling ready-ish

---
+++ src/components/EditionCard.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let name;
+  export let env;
+</script>
+
+<!-- todo the shadow disappears in dark mode + logos -->
+<div
+  class="text-4xl min-w-[24rem] bg-blue-300 p-8 py-12 relative z-10 shadow-ubrut"
+>
+  <div class="text-indigo-900">{name}</div>
+  <div class="text-indigo-500">{env}</div>
+</div>
---
+++ src/components/ImageChooser.svelte
@@ -0,0 +1,16 @@
+<script>
+  import EditionCard from "./EditionCard.svelte";
+</script>
+
+<div
+  class="w-full max-w-7xl mx-auto h-full flex flex-row flex-wrap justify-center items-center p-16 gap-16"
+>
+  <EditionCard name="Silverblue" env="GNOME" />
+  <EditionCard name="Kinoite" env="KDE Plasma" />
+  <EditionCard name="Sericea" env="Sway" />
+  <EditionCard name="Vauxite" env="XFCE" />
+  <EditionCard name="Lazurite" env="LXQt" />
+  <EditionCard name="Onyx" env="Budgie" />
+  <EditionCard name="Mate" env="Mate" />
+  <EditionCard name="Base" env="No DE" />
+</div>
