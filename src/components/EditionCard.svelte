8e3d6dc514d25863136003b71fdc3946a2ab9398
ER <eino.rauhala@proton.me>
2023-05-18 16:12:25 +0300

    chore: switch to Box in EditionCard

--- src/components/EditionCard.svelte
+++ src/components/EditionCard.svelte
@@ -1,12 +1,12 @@
 <script>
   export let name;
   export let env;
+
+  import Box from "@components/Box.svelte";
 </script>
 
-<!-- todo the shadow disappears in dark mode + logos -->
-<div
-  class="text-4xl min-w-[24rem] bg-bg-primary p-10 py-12 relative z-10 shadow-ubrut"
->
+<!-- todo logos -->
+<Box class="text-4xl min-w-[24rem] p-10 py-12">
   <div class="text-fg-primary">{name}</div>
   <div class="text-fg-secondary">{env}</div>
-</div>
+</Box>
