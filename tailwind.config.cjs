e987e02351410a9ebc3ea759ffcd831620da225c
ER <eino.rauhala@proton.me>
2023-05-18 14:49:36 +0300

    feat: basic "imagechooser" w list of editions no functionality yet, but styling ready-ish

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,11 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        ubrut: "0.25rem 0.25rem 0px 0px rgb(30 64 175 / var(--tw-bg-opacity))",
+      },
+    },
   },
   plugins: [],
 };
