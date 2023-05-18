aa35e4f9ffdc7b75e28d4ba099b6ca3763962198
ER <eino.rauhala@proton.me>
2023-05-18 15:38:27 +0300

    refactor: restyle "Box" element

--- src/components/Box.svelte
+++ src/components/Box.svelte
@@ -1,25 +1,6 @@
 <script lang="ts">
-  import { darkModeStore } from "@ts/stores";
-  export let dark: boolean = false;
-
-  let darkMode: boolean;
-  darkModeStore.subscribe((d) => {
-    darkMode = d;
-  });
-
-  $: defaultClasses = getDefaultClasses(darkMode);
-
-  function getDefaultClasses(darkMode: boolean) {
-    let defaultClasses: string = "outline outline-2 p-2";
-    if (dark || darkMode) {
-      defaultClasses +=
-        " bg-slate-800 text-white hover:outline-indigo-300 focus:outline-indigo-300 outline-indigo-100 dark:outline-indigo-900 dark:hover:outline-indigo-600 dark:focus:outline-indigo-600";
-    } else {
-      defaultClasses +=
-        " bg-gray-100 text-black hover:outline-indigo-300 focus:outline-indigo-300 outline-indigo-100 dark:outline-indigo-900 dark:hover:outline-indigo-600 dark:focus:outline-indigo-600";
-    }
-    return defaultClasses;
-  }
+  let defaultClasses: string =
+    "p-2 bg-bg-primary text-fg-primary shadow-ubrut-sm hover:shadow-ubrut focus:shadow-ubrut";
 </script>
 
 <div class={`${defaultClasses} ${$$props.class}`}>
