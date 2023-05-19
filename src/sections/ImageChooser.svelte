9d3ca2dae59a46ecea24bb6e2cfa18bf98180747
ER <eino.rauhala@proton.me>
2023-05-19 18:36:49 +0300

    chore: add todo

--- src/sections/ImageChooser.svelte
+++ src/sections/ImageChooser.svelte
@@ -2,6 +2,7 @@
   import EditionCard from "@components/EditionCard.svelte";
 </script>
 
+<!-- todo get data from content.yml -->
 <div
   class="w-full max-w-7xl mx-auto h-full flex flex-row flex-wrap justify-center items-center p-16 gap-16"
 >
