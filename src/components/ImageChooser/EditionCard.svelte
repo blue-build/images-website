be2d4fc2aa823b32c44b00afe123a8d2cba99406
ER <eino.rauhala@proton.me>
2023-05-20 16:16:53 +0300

    feat: add basic image chooser / creator UI chooser is basically ready and working, but creator is non-functional

---
+++ src/components/ImageChooser/EditionCard.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  export let name: string;
+  export let notice: string | undefined = undefined;
+
+  import Box from "@components/Box.svelte";
+
+  import { imagePreferences } from "@ts/stores";
+  import { MainEdition } from "@ts/types";
+
+  let selected: boolean;
+  imagePreferences.subscribe((pref) => {
+    if (pref?.edition != undefined) {
+      selected = pref.edition?.name === name;
+    }
+  });
+
+  function select() {
+    imagePreferences.update((pref) => {
+      return {
+        ...pref,
+        edition: MainEdition.all.find((edi) => edi.name == name),
+      };
+    });
+    window.location.replace("/#nvidia");
+  }
+</script>
+
+<!-- todo logos -->
+<button
+  on:click={select}
+  class={`text-left
+  ${selected ? "outline outline-2 outline-bg-secondary shadow-ubrut-lg" : ""}`}
+>
+  <Box class="text-4xl w-[24rem] h-[12rem] p-10 flex flex-col justify-center">
+    <div class="text-fg-primary">{name}</div>
+    <div class="text-fg-secondary">
+      {MainEdition.all.find((edi) => edi.name == name)?.desktop}
+    </div>
+    {#if notice != undefined}
+      <div class="text-fg-secondary text-sm mt-1">{notice}</div>
+    {/if}
+  </Box>
+</button>
---
+++ src/components/ImageChooser/Nvidia.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import Box from "@components/Box.svelte";
+  import { imagePreferences } from "@ts/stores";
+
+  let nvidia: boolean;
+  imagePreferences.subscribe((pref) => {
+    if (pref?.nvidia != undefined) {
+      nvidia = pref?.nvidia;
+    }
+  });
+
+  function set(nvidia: boolean) {
+    imagePreferences.update((pref) => {
+      return {
+        ...pref,
+        nvidia: nvidia,
+      };
+    });
+    window.location.replace("/#images");
+  }
+</script>
+
+<div id="nvidia" class="w-full max-w-4xl mx-auto p-16">
+  <Box class="p-8 text-xl flex gap-8">
+    <div class="my-auto mr-9">Do you want Nvidia drivers?</div>
+    <button
+      on:click={() => {
+        set(true);
+      }}
+    >
+      <Box
+        class={`bg-bg-primary p-4 w-32 outline outline-bg-secondary shadow-ubrut hover:shadow-ubrut-lg ${
+          nvidia ? "outline-2 !shadow-ubrut-lg" : "outline-1"
+        }`}
+      >
+        Yes
+      </Box>
+    </button>
+    <button
+      on:click={() => {
+        set(false);
+      }}
+    >
+      <Box
+        class={`bg-bg-primary p-4 w-32 outline outline-bg-secondary shadow-ubrut hover:shadow-ubrut-lg ${
+          !nvidia ? "outline-2 !shadow-ubrut-lg" : "outline-1"
+        }`}
+      >
+        No
+      </Box>
+    </button>
+  </Box>
+</div>
