f2fd8ac2d772b37482277ec96da6103799fd8d15
ER <eino.rauhala@proton.me>
2023-05-26 19:38:18 +0300

    chore: add "border" option to Box

--- src/components/Box.svelte
+++ src/components/Box.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-  let defaultClasses: string =
+  let defaultClasses =
     "bg-bg-primary text-fg-primary shadow-ubrut-sm hover:shadow-ubrut focus:shadow-ubrut";
+  export let border: boolean = false;
+  let borderClasses = border ? "outline outline-1 outline-bg-secondary" : "";
 </script>
 
-<div class={`${defaultClasses} ${$$props.class}`}>
+<div class={`${defaultClasses} ${borderClasses} ${$$props.class}`}>
   <slot />
 </div>
