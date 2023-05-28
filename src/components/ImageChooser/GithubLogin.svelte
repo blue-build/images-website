ccaea7ac70293aedd7e7620dbfde737be7644d29
ER <eino.rauhala@proton.me>
2023-05-28 14:28:52 +0300

    fix: misc changes, gh app to oauth app, save login

--- src/components/ImageChooser/GithubLogin.svelte
+++ src/components/ImageChooser/GithubLogin.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
   import Box from "@components/Box.svelte";
   import { customImage } from "@ts/stores";
-  import type { CustomImage } from "@ts/types";
   import { onMount } from "svelte";
 
   let callback = "";
   let authorized = false;
   let auth = "";
-  let userInfo;
+  let userInfo: { login: string };
 
   // TODO auth state is not persistent
   customImage.subscribe((c) => {
@@ -19,6 +18,7 @@
     const urlParams = new URLSearchParams(window.location.search);
     authorized = urlParams.has("access_token");
     if (authorized) {
+      // @ts-expect-error // "access_token" not being null is checked by if
       auth = urlParams.get("access_token");
       customImage.update((c) => {
         return {
@@ -35,24 +35,32 @@
       headers: { Authorization: `Bearer ${auth}` },
     });
     userInfo = await res.json();
+    customImage.update((c) => {
+      return {
+        ...c,
+        login: userInfo.login,
+      };
+    });
   }
 
   // This is not sensitive and Vite only reads envvars from .env
   // so it wouldn't be possible to use this value from a Netlify envvar
-  const clientId = "Iv1.1bcc0e7bd16639c1";
+  const clientId = "8c6add6c853b329dd78c";
 </script>
 
 <div id="login" class="w-full max-w-4xl mx-auto p-16">
   <Box class="p-8 text-xl">
     {#if !authorized}
-      <a href="https://github.com/login/oauth/authorize?client_id={clientId}">
+      <a
+        href="https://github.com/login/oauth/authorize?client_id={clientId}&scope=repo"
+      >
         <Box border class="p-4 w-fit">Login to GitHub.</Box>
       </a>
     {:else}
       <Box border class="p-4 w-fit">
         Logged in {#if userInfo}
           as {userInfo.login}
-        {/if}!
+        {/if}
       </Box>
     {/if}
   </Box>
